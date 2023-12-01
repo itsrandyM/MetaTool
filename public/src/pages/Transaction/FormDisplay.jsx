5d90f3cb51b35fcc8dee20318c8ca2543222a146
itsrandymungai <randymungai20@gmail.com>
2023-12-01 18:19:01 +0300

    merge conflicts and changes

--- public/src/pages/Transaction/FormDisplay.jsx
+++ public/src/pages/Transaction/FormDisplay.jsx
@@ -4,7 +4,6 @@ import Foot from './Foot/foot';
 import Form1 from './Form1/Form1';
 import Form2 from './Form2/Form2';
 import Form3 from './Form3/Form3';
-import Form4 from './Form4/Form4';
 
 const FormDisplay = () => {
   const [currentForm, setCurrentForm] = useState(1);
@@ -21,8 +20,6 @@ const FormDisplay = () => {
         return <Form2 onNextForm={handleNextForm} />;
       case 3:
         return <Form3 onNextForm={handleNextForm} />;
-      case 4:
-        return <Form4 />;
       default:
         return <Form1 onNextForm={handleNextForm} />;
     }
--- server/routes/Transaction.js
+++ server/routes/Transaction.js
@@ -7,46 +7,6 @@ const Description = require('../models/Description')
 //const { route } = require('./root')
 
 
-// Add recipient route
-router.post('/addRecipient', async (req, res, next) => {
-  try {
-    const { name, email, walletAddress } = req.body;
-    const user = req.user; // Authenticate
-
-    const recipient = await Recipient.create({ User: user, name, email, walletAddress });
-
-    res.json({ success: true, recipient });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
-  }
-});
-
-// Add recipient data / Transaction details route
-router.post('/addRecipientTransaction', async (req, res, next) => {
-  try {
-    const { name, tokenName, descriptionName, classificationName } = req.body;
-    const user = req.user; // Authenticate later
-
-    const token = await Token.findOneOrCreate({ user: user, tokenName });
-    const description = await Description.findOneOrCreate({ user: user, descriptionName });
-    const classification = await Classification.findOneOrCreate({ user: user, classificationName });
-
-    const recipientsData = await RecipientsData.create({
-      User: user,
-      name,
-      token,
-      description,
-      classification,
-    });
-
-    res.json({ success: true, recipientsData });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: 'Internal Server Error' });
-  }
-});
-
 // Admin verification of data
 router.post('/verifyData', async (req, res, next) => {
   try {
