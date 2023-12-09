b77eeb3fdb2c4481e83ff98ab5d29c52d944bda2
raymond <raymondezra276@gmail.com>
2023-12-09 10:37:36 +0300

    update

--- public/src/pages/Wallets/wallets.jsx
+++ public/src/pages/Wallets/wallets.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/navbar'
 import './style.css'
-import SuccessPage from '../Transaction/Success/Success'
 
 const Wallets = () => {
   return (
     <div className='wallets'>
         <Navbar/>
         <div>
-    <SuccessPage/>
         </div>
       </div>
   )
