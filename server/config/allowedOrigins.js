2775b904c8cc88967e48df330fedfd181508caa4
itsrandyM <134689399+itsrandyM@users.noreply.github.com>
2023-12-12 11:01:26 +0300

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:4000',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://meta-tool.vercel.app'
 ]
 
 module.exports = allowedOrigins
