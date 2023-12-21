a6ce07fa4f24a83dc03c1a44029f57732c5662b4
raymond <raymondezra276@gmail.com>
2023-12-21 10:47:59 +0300

    update

--- public/src/pages/Support/support.jsx
+++ public/src/pages/Support/support.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import './style.css'
+import ExchangeRateForm from '../Transaction/exchange/exchange'
 
 
 const Support = () => {
   return (
     <div className='main'>
     <Navbar/>
+    <ExchangeRateForm/>
     <div className='main__content'>
     </div>
     </div>
