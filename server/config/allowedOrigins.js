365cdda5b8cb0c3b0b350cc86e5c01b861335808
Ray-Ezra <134621588+Ray-Ezra@users.noreply.github.com>
2024-01-03 21:34:29 +0300

    update

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
     'http://localhost:10000',
-    'https://meta-s.onrender.com',
+    'http://meta-s.onrender.com',
     'http://localhost:5173',
-    'https://meta-tool-iota.vercel.app/'
+    'http://meta-tool-iota.vercel.app/'
 ]
 
 module.exports = allowedOrigins
