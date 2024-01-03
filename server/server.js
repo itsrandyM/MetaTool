d5c52ec12b2d66b293189d572c6957d283440a87
itsrandyM <134689399+itsrandyM@users.noreply.github.com>
2024-01-03 17:44:39 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,6 +17,12 @@ const AuthRouter = require('./routes/AuthRoutes')
 
 app.use(logger)
 app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://meta-tool-iota.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+})
 app.use(express.json())
 app.use(cookieParser())
 
