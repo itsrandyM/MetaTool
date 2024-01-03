4b901300e55cca83f27b377b73ecca82a71afcd1
itsrandymungai <randymungai20@gmail.com>
2024-01-03 17:13:57 +0300

    deployment links

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
-    'http://localhost:4000',
+    'http://localhost:10000',
+    'https://meta-s.onrender.com',
     'http://localhost:5173',
+    'https://meta-tool-iota.vercel.app/'
 ]
 
 module.exports = allowedOrigins
