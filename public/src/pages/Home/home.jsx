39ccadc4eedb877ee405ca9724f85ba7cbaf3f6c
Ray-Ezra <raymondezra276@gmail.com>
2024-03-12 17:01:51 +0300

    fixed

--- public/src/pages/Home/home.jsx
+++ public/src/pages/Home/home.jsx
@@ -7,7 +7,7 @@ const WelcomePage = () => {
   return (
     <>
       <Navbar />
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', padding: '0 20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '100vh', padding: '0 20px' }}>
         <h2 style={{ color: 'black', fontSize: '20px', textAlign: 'center' }}>Welcome to Meta Tool</h2>
         <div style={{ maxWidth: '100%', overflowX: 'auto', marginTop: '20px', width: '100%' }}>
           <Table />
